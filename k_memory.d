c734538c01e4f61257a66719e323789a9063e5c1
David Fu <davidf1212@gmail.com>
2016-02-05 09:00:59 -0500

    remove ignored files

--- k_memory.d
+++
@@ -1,5 +0,0 @@
-k_memory.o: src\k_memory.c
-k_memory.o: src\k_memory.h
-k_memory.o: src\k_rtx.h
-k_memory.o: src\printf.h
-k_memory.o: C:\Keil\ARM\ARMCC\bin\..\include\stdarg.h
--- usr_proc.d
+++
@@ -1,8 +0,0 @@
-usr_proc.o: src\usr_proc.c
-usr_proc.o: src\rtx.h
-usr_proc.o: src\uart_polling.h
-usr_proc.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-usr_proc.o: src\uart_def.h
-usr_proc.o: src\usr_proc.h
-usr_proc.o: src\printf.h
-usr_proc.o: C:\Keil\ARM\ARMCC\bin\..\include\stdarg.h
