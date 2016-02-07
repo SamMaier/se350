84e1f276bdad735f599a5d04bf793bc30a886fcb
Hamdan Javeed <hamdan.javeed@uwaterloo.ca>
2016-02-07 18:59:07 -0500

    Add a system proc file, create a null process

---
+++ src/k_sys_proc.c
@@ -0,0 +1,30 @@
+/**
+ * @file:   k_sys_proc.c
+ * @brief:  System processes: null process
+ */
+
+#include "k_rtx.h"
+#include "k_sys_proc.h"
+
+#ifdef DEBUG_0
+#include "printf.h"
+#endif /* DEBUG_0 */
+
+extern int k_release_processor(void);
+
+/* initialization table */
+PROC_INIT g_system_procs[NUM_SYSTEM_PROCS];
+
+void set_system_procs() {
+    /* null process */
+    g_system_procs[0].m_pid = 0;
+    g_system_procs[0].m_priority = 4;
+    g_system_procs[0].m_stack_size = 0x100;
+    g_system_procs[0].mpf_start_pc = &null_process;
+}
+
+void null_process() {
+    while (1) {
+        k_release_processor();
+    }
+}
---
+++ src/k_sys_proc.h
@@ -0,0 +1,12 @@
+/**
+ * @file:   k_sys_proc.h
+ * @brief:  System processes header file
+ */
+
+#ifndef SYS_PROC_H_
+#define SYS_PROC_H_
+
+void set_system_procs(void);
+void null_process(void);
+
+#endif /* SYS_PROC_H_ */
