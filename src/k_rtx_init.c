cb3bf8da4296207ad7c52d54a17615950eae8c62
Hamdan Javeed <hamdan.javeed@uwaterloo.ca>
2016-03-04 22:41:26 -0500

    Add the timer handler

--- src/k_rtx_init.c
+++ src/k_rtx_init.c
@@ -10,18 +10,20 @@
 #include "uart.h"
 #include "k_memory.h"
 #include "k_process.h"
+#include "k_timer.h"
 
 void k_rtx_init(void)
 {
-	__disable_irq();
-	uart_irq_init(0);   // uart0, interrupt-driven 
-	uart1_init();       // uart1, polling
-	memory_init();
-	process_init();
-	__enable_irq();
-	
-	uart1_put_string("Type 'S' in COM0 terminal to switch between proc1 and proc2 or wait for them to switch between themselves\n\r");
-	uart1_put_string("An input other than 'S' in COM0 terminal will be have no effect.\n\r"); 
-	/* start the first process */
-	k_release_processor();
+    __disable_irq();
+    uart_irq_init(0);   // uart0, interrupt-driven
+    uart1_init();       // uart1, polling
+    memory_init();
+    process_init();
+    timer_init(0);
+    __enable_irq();
+
+    uart1_put_string("Type 'S' in COM0 terminal to switch between proc1 and proc2 or wait for them to switch between themselves\n\r");
+    uart1_put_string("An input other than 'S' in COM0 terminal will be have no effect.\n\r");
+    /* start the first process */
+    k_release_processor();
 }
