a656a17ebb130cd5b23f4cee85b84e944b42af20
David Fu <davidf1212@gmail.com>
2016-03-06 22:22:46 -0500

    fixed queue logic

--- src/k_sys_proc.c
+++ src/k_sys_proc.c
@@ -19,7 +19,6 @@ PROC_INIT g_sys_procs[NUM_SYS_PROCS];
 
 /* timer */
 MSG* timeout_queue_front = NULL;
-MSG* timeout_queue_back = NULL;
 
 void set_sys_procs() {
     /* null process */
@@ -42,103 +41,39 @@ void null_process() {
 }
 
 void enqueue_message_delayed(PCB* pcb, MSG* message, int delay) {
-    MSG* currentMessage;
-    MSG* nextMessage;
+    MSG* current = timeout_queue_front;
 
     int expiry = get_sys_time() + delay;
     message->m_expiry = expiry;
 
-    currentMessage = timeout_queue_front;
-
-    if (currentMessage == NULL) {
-        // no messages in the queue
-        message->mp_next = NULL;
-        message->mp_prev = NULL;
-
+    if (current == NULL || current->m_expiry >= expiry) {
+        message->mp_next = current;
         timeout_queue_front = message;
-        timeout_queue_back = message;
-    } else if (timeout_queue_front == timeout_queue_back) {
-        // one message in the queue
-        if (currentMessage->m_expiry <= message->m_expiry) {
-            // insert message after currentMessage
-            currentMessage->mp_next = message;
-            message->mp_prev = currentMessage;
-
-            timeout_queue_back = message;
-        } else {
-            // insert message before currentMessage
-            message->mp_next = currentMessage;
-            currentMessage->mp_prev = message;
-
-            timeout_queue_front = message;
-        }
     } else {
-        // queue has at least 2 messages
-
-        if (message->m_expiry <= currentMessage->m_expiry) {
-            // insert message before currentMessage
-            message->mp_next = currentMessage;
-            currentMessage->mp_prev = message;
-
-            timeout_queue_front = message;
-        } else {
-            // insert message either in the middle or at the end of the queue
-            nextMessage = currentMessage->mp_next;
-
-            while (nextMessage != NULL && nextMessage->m_expiry < message->m_expiry) {
-                currentMessage = nextMessage;
-                nextMessage = currentMessage->mp_next;
-            }
-
-            // insert message after currentMessage
-            currentMessage->mp_next = message;
-            message->mp_prev = currentMessage;
-
-            if (nextMessage != NULL) {
-                // inserting message in the middle of the queue
-                message->mp_next = nextMessage;
-                nextMessage->mp_prev = message;
-            } else {
-                // inserting message at the end of the queue
-                message->mp_next = NULL;
-
-                timeout_queue_back = message;
-            }
+        while (current->mp_next != NULL && current->mp_next->m_expiry < expiry) {
+            current = current->mp_next;
         }
+        message->mp_next = current->mp_next;
+        current->mp_next = message;
     }
 }
 
-void remove_message_delayed(MSG *message) {
-    MSG* previousMessage;
+void dequeue_message_delayed(MSG *message) {
+    MSG* current = timeout_queue_front;
+    MSG* previous;
 
-    if (timeout_queue_front == NULL) {
-        // queue is empty, don't do anything
-    } else if (timeout_queue_front == timeout_queue_back && timeout_queue_front == message) {
-        // message is the only element in the queue
-        timeout_queue_front = NULL;
-        timeout_queue_back = NULL;
-
-        message->mp_next = NULL;
-        message->mp_prev = NULL;
-    } else {
-        // queue has at least 2 elements
-        if (timeout_queue_front == message) {
-            // message is the first element
-            timeout_queue_front = message->mp_next;
-
-            message->mp_next = NULL;
-        } else if (timeout_queue_back == message) {
-            // message is the last element
-            timeout_queue_back = message->mp_prev;
-
-            message->mp_prev = NULL;
+    while (current != NULL) {
+        if (current == message) {
+            if (current == timeout_queue_front) {
+                timeout_queue_front = current->mp_next;
+                // deq curr
+            } else {
+                previous->mp_next = current->mp_next;
+                // deq curr
+            }
         } else {
-            // message is somewhere in the middle
-            previousMessage = message->mp_prev;
-            previousMessage->mp_next = message->mp_next;
-
-            message->mp_prev = NULL;
-            message->mp_next = NULL;
+            previous = current;
+            current = current->mp_next;
         }
     }
 }
@@ -152,7 +87,7 @@ void timer_i_process() {
 
     while (message != NULL) {
         if (message->m_expiry <= systemTime) {
-            remove_message_delayed(message);
+            dequeue_message_delayed(message);
             k_send_message(message->m_receive_id, message);
             message = message->mp_next;
         } else {
