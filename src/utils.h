01e9b0a63c72137b01ed32f511cd6c344244427e
Hamdan Javeed <hamdan.javeed@uwaterloo.ca>
2016-03-18 19:31:39 -0400

    utils.h

--- src/utils.h
+++ src/utils.h
@@ -1,7 +1,36 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-//void strcpy(char* dest, const char* src) {
-//    while (*dest++ = *src++);
-//}
+#ifdef DEBUG_0
+    #include "printf.h"
+    #include "stdarg.h"
+#endif
+
+void log(char* format, ...) {
+#ifndef DEBUG_0
+    return;
+#endif
+
+    va_list args;
+    va_start(args, format);
+
+    printf(format, args);
+
+    va_end(args);
+}
+
+void logln(char* format, ...) {
+#ifndef DEBUG_0
+    return;
+#endif
+
+    va_list args;
+    va_start(args, format);
+
+    log(format, args);
+    printf("\n");
+
+    va_end(args);
+}
+
 #endif
