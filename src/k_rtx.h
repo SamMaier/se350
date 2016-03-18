41fe4c0191300a2a44c25d059f9b880a33e4dffd
Hamdan Javeed <hamdan.javeed@uwaterloo.ca>
2016-03-18 19:00:26 -0400

    k_rtx.h

--- src/k_rtx.h
+++ src/k_rtx.h
@@ -1,12 +1,12 @@
-#ifndef K_RTX_H_
-#define K_RTX_H_
+#ifndef K_RTX_H
+#define K_RTX_H
 
 #include "common.h"
 
 #ifdef DEBUG_0
-#define USR_SZ_STACK 0x200 // user proc stack size 512B
+    #define USR_SZ_STACK 0x200 // user proc stack size 512B
 #else
-#define USR_SZ_STACK 0x100 // user proc stack size 218B
+    #define USR_SZ_STACK 0x100 // user proc stack size 128B
 #endif // DEBUG_0
 
-#endif // K_RTX_H_
+#endif // K_RTX_H
