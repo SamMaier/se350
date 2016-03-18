927486e60721523dcae16000c315b622861cbf21
Hamdan Javeed <hamdan.javeed@uwaterloo.ca>
2016-03-18 18:56:33 -0400

    k_rtx_init.c

--- src/k_rtx_init.c
+++ src/k_rtx_init.c
@@ -1,27 +1,30 @@
-/** 
- * @file:   k_rtx_init.c
- * @brief:  Kernel initialization C file
- * @auther: Yiqing Huang
- * @date:   2014/01/17
- */
-
 #include "k_rtx_init.h"
-#include "uart_polling.h"
 #include "uart.h"
 #include "k_memory.h"
 #include "k_process.h"
 #include "k_timer.h"
 
-void k_rtx_init(void)
-{
+#ifdef DEBUG_0
+    #include "uart_polling.h"
+#endif
+
+void k_rtx_init(void) {
     __disable_irq();
+
+    /* UART */
     uart_irq_init(0);   // uart0, interrupt-driven
+
+#ifdef DEBUG_0
     uart1_init();       // uart1, polling
+#endif
+
+    /* Kernel */
     memory_init();
     process_init();
     timer_init(0);
+
     __enable_irq();
 
-    /* start the first process */
+    // start the first process
     k_release_processor();
 }
