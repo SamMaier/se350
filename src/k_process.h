37e7a97ebeed012b077c50e84f499036cee27018
Hamdan Javeed <hamdan.javeed@uwaterloo.ca>
2016-03-19 00:23:21 -0400

    k_process.h

--- src/k_process.h
+++ src/k_process.h
@@ -4,21 +4,21 @@
 #include "k_rtx.h"
 
 /* Definitions */
-#define INITIAL_xPSR 0x01000000 // user process initial xPSR value
+#define INITIAL_xPSR 0x01000000 // user process initial xPSR (Program Status Register) value
 
 /* Functions */
-void process_init(void);                          // initialize all procs in the system
-PCB* scheduler(void);                             // pick the pid of the next to run process
-int k_release_process(void);                      // kernel release_process function
-int k_set_process_priority(const int, const int); // set process priority
-int k_get_process_priority(const int);            // get process priority
-int k_send_message_envelope(int process_id, void* message_envelope);
+void process_init(void);
+PCB* scheduler(void);
+int k_release_process(void);
+int k_set_process_priority(const int, const int);
+int k_get_process_priority(const int);
+int k_send_message_envelope(int process_id, void* p_msg_envelope);
 void* k_receive_message(int* sender_id);
-int k_delayed_send(int process_id, void* message_envelope, int delay);
+int k_delayed_send(int process_id, void* p_msg_envelope, int delay);
 
 extern U32* alloc_stack(U32 size_b); // allocate stack for a process
 extern void __rte(void);             // pop exception stack frame
-extern void set_test_procs(void);    // test process initial set up
-extern void set_sys_procs(void);     // system process initial set up
+extern void set_test_procs(void);
+extern void set_sys_procs(void);
 
 #endif // K_PROCESS_H
